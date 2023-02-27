6a3b1e5ed42d85fed135ee5ec8db018d41f3604c
engadnanhossain <engr.adnanhossain@gmail.com>
2023-02-27 22:05:16 +0600

    Added deposit file

--- js/deposit.js
+++ js/deposit.js
@@ -15,3 +15,20 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
 
 
 })
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawAmount = document.getElementById('withdraw-field');
+    const withdraw = parseFloat(withdrawAmount.value);
+    const addWithdraw = document.getElementById('withdraw-amount');
+    const previousWithdraw = parseFloat(addWithdraw.innerText);
+    const totalWithdraw = withdraw + previousWithdraw;
+    addWithdraw.innerText = totalWithdraw;
+    withdrawAmount.value = '';
+
+
+    //Balance Withdraw
+    const mainBalance = document.getElementById('main-balance');
+    const previousMainBalance = parseFloat(mainBalance.innerText);
+    const totalMainBalance = previousMainBalance - withdraw;
+    mainBalance.innerText = totalMainBalance;
+})
