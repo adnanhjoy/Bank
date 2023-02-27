1828f686e4ef36bb2c056eb8555541408e01c6d0
engadnanhossain <engr.adnanhossain@gmail.com>
2023-02-27 23:48:21 +0600

    Added withdraw file

---
+++ js/withdraw.js
@@ -0,0 +1,16 @@
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawAmount = document.getElementById('withdraw-field');
+    const withdraw = parseFloat(withdrawAmount.value);
+    const addWithdraw = document.getElementById('withdraw-amount');
+    const previousWithdraw = parseFloat(addWithdraw.innerText);
+    const totalWithdraw = withdraw + previousWithdraw;
+    addWithdraw.innerText = totalWithdraw;
+    withdrawAmount.value = '';
+
+
+    //Balance Withdraw
+    const mainBalance = document.getElementById('main-balance');
+    const previousMainBalance = parseFloat(mainBalance.innerText);
+    const totalMainBalance = previousMainBalance - withdraw;
+    mainBalance.innerText = totalMainBalance;
+})
